a4aa939270339e05fcc346049c79ef19bd846acf
Edwin Gonzalez <edgoca1962@gmail.com>
2026-09-15 08:27:12 -0600

    logo navbar

--- core/views/navbar.php
+++ core/views/navbar.php
@@ -24,21 +24,20 @@ $location = MenuResolver::get_instance()->location();
 <nav class="navbar navbar-expand-lg bg-transparent fixed-top">
     <div class="container-fluid">
         <a class="navbar-brand" href="<?php echo esc_url(home_url('/')); ?>">
-            <?php if (has_custom_logo()) :
+            <?php if (has_custom_logo()):
                 echo wp_get_attachment_image(get_theme_mod('custom_logo'), 'full', false, [
-                    'id'    => 'site-logo',
-                    'width' => 28,
-                    'height' => 28,
-                    'style' => 'width:28px;height:28px;object-fit:contain;',
+                    'id' => 'site-logo',
+                    'width' => 60,
+                    'height' => 60,
+                    'style' => 'object-fit:contain;',
                 ]);
-            else :
+            else:
                 bloginfo('name');
             endif; ?>
         </a>
 
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#egcNavbar" aria-controls="egcNavbar"
-                aria-expanded="false" aria-label="<?php esc_attr_e('Menú', 'egc'); ?>">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#egcNavbar"
+            aria-controls="egcNavbar" aria-expanded="false" aria-label="<?php esc_attr_e('Menú', 'egc'); ?>">
             <span class="navbar-toggler-icon"></span>
         </button>
 
@@ -46,21 +45,21 @@ $location = MenuResolver::get_instance()->location();
             <?php
             wp_nav_menu([
                 'theme_location' => $location,
-                'container'      => false,
-                'items_wrap'     => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-                'menu_class'     => 'navbar-nav me-auto mb-2 mb-lg-0',
-                'walker'         => new BootstrapNavWalker(),
-                'fallback_cb'    => false,
+                'container' => false,
+                'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+                'menu_class' => 'navbar-nav me-auto mb-2 mb-lg-0',
+                'walker' => new BootstrapNavWalker(),
+                'fallback_cb' => false,
             ]);
             ?>
 
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item dropdown">
-                    <?php if (is_user_logged_in()) : ?>
+                    <?php if (is_user_logged_in()): ?>
                         <a class="nav-link dropdown-toggle d-flex align-items-center" href="#" role="button"
-                           data-bs-toggle="dropdown" aria-expanded="false">
-                            <img src="<?php echo esc_url(get_avatar_url(get_current_user_id())); ?>" alt=""
-                                 width="28" height="28" class="rounded-circle border border-2" style="object-fit:cover;">
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            <img src="<?php echo esc_url(get_avatar_url(get_current_user_id())); ?>" alt="" width="28"
+                                height="28" class="rounded-circle border border-2" style="object-fit:cover;">
                         </a>
                         <ul class="dropdown-menu dropdown-menu-end">
                             <li>
@@ -69,13 +68,15 @@ $location = MenuResolver::get_instance()->location();
                                 </a>
                             </li>
                             <li>
-                                <a class="dropdown-item" href="<?php echo esc_url(PasswordChange::get_instance()->url()); ?>">
+                                <a class="dropdown-item"
+                                    href="<?php echo esc_url(PasswordChange::get_instance()->url()); ?>">
                                     <?php esc_html_e('Cambio contraseña', 'egc'); ?>
                                 </a>
                             </li>
-                            <?php if (UserScope::get_instance()->is_general_admin() || !empty(UserScope::get_instance()->managed_post_types())) : ?>
+                            <?php if (UserScope::get_instance()->is_general_admin() || !empty(UserScope::get_instance()->managed_post_types())): ?>
                                 <li>
-                                    <a class="dropdown-item" href="<?php echo esc_url(UserManagement::get_instance()->url()); ?>">
+                                    <a class="dropdown-item"
+                                        href="<?php echo esc_url(UserManagement::get_instance()->url()); ?>">
                                         <?php esc_html_e('Gestión de usuarios', 'egc'); ?>
                                     </a>
                                 </li>
@@ -89,11 +90,11 @@ $location = MenuResolver::get_instance()->location();
                                 </a>
                             </li>
                         </ul>
-                    <?php else : ?>
+                    <?php else: ?>
                         <a class="nav-link dropdown-toggle d-flex align-items-center" href="#" role="button"
-                           data-bs-toggle="dropdown" aria-expanded="false">
+                            data-bs-toggle="dropdown" aria-expanded="false">
                             <img src="<?php echo esc_url(Account::get_instance()->generic_avatar_url()); ?>" alt=""
-                                 width="28" height="28" class="rounded-circle border border-2" style="object-fit:cover;">
+                                width="28" height="28" class="rounded-circle border border-2" style="object-fit:cover;">
                         </a>
                         <ul class="dropdown-menu dropdown-menu-end">
                             <li>
@@ -102,7 +103,8 @@ $location = MenuResolver::get_instance()->location();
                                 </a>
                             </li>
                             <li>
-                                <a class="dropdown-item" href="<?php echo esc_url(UserRegistration::get_instance()->url()); ?>">
+                                <a class="dropdown-item"
+                                    href="<?php echo esc_url(UserRegistration::get_instance()->url()); ?>">
                                     <?php esc_html_e('Solicitar ingreso', 'egc'); ?>
                                 </a>
                             </li>
